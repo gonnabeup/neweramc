2655faa167f0293ac9635b517949260412bc418b
NikolenkoDV <nikolenkodv@mer.ci.nsu.ru>
2025-05-03 21:35:22 +0700

    fc

--- src/Miningcore/Blockchain/SpaceMvc/SpaceMvcModule.cs
+++ src/Miningcore/Blockchain/SpaceMvc/SpaceMvcModule.cs
@@ -3,6 +3,26 @@ using Miningcore.Blockchain;
 using Miningcore.Blockchain.Bitcoin;
 using Miningcore.Mining;
 using Miningcore.Payments;
+using Autofac;
+using AutoMapper;
+using Miningcore.Blockchain.Bitcoin;
+using Miningcore.Blockchain.Bitcoin.Configuration;
+using Miningcore.Blockchain.Bitcoin.DaemonResponses;
+using Miningcore.Blockchain.SpaceMvc.Configuration;
+using Miningcore.Configuration;
+using Miningcore.Extensions;
+using Miningcore.Messaging;
+using Miningcore.Mining;
+using Miningcore.Payments;
+using Miningcore.Persistence;
+using Miningcore.Persistence.Model;
+using Miningcore.Persistence.Repositories;
+using Miningcore.Rpc;
+using Miningcore.Time;
+using Miningcore.Util;
+using Block = Miningcore.Persistence.Model.Block;
+using Contract = Miningcore.Contracts.Contract;
+using static Miningcore.Util.ActionUtils;
 
 namespace Miningcore.Blockchain.SpaceMvc;
 
